a956c44b2f2f09f04bf5f7189a558e3659c78193
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-26 14:11:38 +0100

    seek to offset

--- kafka_consume.py
+++ kafka_consume.py
@@ -9,16 +9,16 @@ def main():
     consumer = KafkaConsumer(
         'V20_writerCommand',
         bootstrap_servers=['localhost:9093'],
-        auto_offset_reset='earliest',
         enable_auto_commit=False,
         value_deserializer=lambda x: loads(x.decode('utf-8')))
+    consumer.seek(partition=None, offset=1210)
 
     for message in consumer:
         # message value and key are raw bytes -- decode if necessary!
         # e.g., for unicode: `message.value.decode('utf-8')`
         print("%s:%d:%d: key=%s value=" % (message.topic, message.partition,
-                                             message.offset, message.key
-                                             ))
+                                           message.offset, message.key
+                                           ))
 
 
 if __name__ == "__main__":
