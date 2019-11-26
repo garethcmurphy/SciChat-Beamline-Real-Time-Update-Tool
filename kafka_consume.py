0a21ced6f795657c5084ec4d14784dc477220c3c
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-26 12:49:48 +0100

    lint errors

--- kafka_consume.py
+++ kafka_consume.py
@@ -6,13 +6,14 @@ from kafka import KafkaConsumer
 def main():
     """setup consumer"""
     consumer = KafkaConsumer(
-    'V20_writerCommand',
-    bootstrap_servers=['localhost:9093'],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='my-group',
-    value_deserializer=lambda x: loads(x.decode('utf-8')))
+        'V20_writerCommand',
+        bootstrap_servers=['localhost:9093'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='my-group',
+        value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 
 if __name__ == "__main__":
     main()
+    
