974462fdaebddb07104c392ebc2acf0dfb544af7
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-26 11:46:06 +0100

    add comeent

--- kafka_consume.py
+++ kafka_consume.py
@@ -3,10 +3,16 @@
 from json import loads
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer(
-    'numtest',
+def main():
+    """setup consumer"""
+    consumer = KafkaConsumer(
+    'v20_filerwriter_Cmd',
     bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
     enable_auto_commit=True,
     group_id='my-group',
     value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    main()
