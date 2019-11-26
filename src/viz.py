303a0f427484d6226375ebb2c40a10f2ddc49f04
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-26 16:21:51 +0100

    lint

--- src/viz.py
+++ src/viz.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
-# """visens"""
+"""visens"""
 import visens
 
-filename = "nicos_00000757.hdf"
-visens.tof(filename, save="/tmp/"+filename.replace(".hdf", ".png"))
+def main():
+    """main"""
+    path = "/data/kafka-to-nexus/"
+    filename = path + "nicos_00000757.hdf"
+    visens.tof(filename, save="/tmp/"+filename.replace(".hdf", ".png"))
+
+if __name__ == "__main__":
+    main()
