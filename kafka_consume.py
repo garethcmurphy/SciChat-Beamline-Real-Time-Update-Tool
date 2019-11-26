d6979c7a13ae3f281d9a95b007ebf1d57b9a5414
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-26 13:01:08 +0100

    print value

--- kafka_consume.py
+++ kafka_consume.py
@@ -16,9 +16,9 @@ def main():
     for message in consumer:
         # message value and key are raw bytes -- decode if necessary!
         # e.g., for unicode: `message.value.decode('utf-8')`
-        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                             message.offset, message.key,
-                                             message.value))
+        print("%s:%d:%d: key=%s value=" % (message.topic, message.partition,
+                                             message.offset, message.key
+                                             ))
 
 
 if __name__ == "__main__":
