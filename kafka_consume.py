76b2e1be86899e558c43eff0ffa8e9b6652e039d
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-26 12:53:37 +0100

    add consummer

--- kafka_consume.py
+++ kafka_consume.py
@@ -3,6 +3,7 @@
 from json import loads
 from kafka import KafkaConsumer
 
+
 def main():
     """setup consumer"""
     consumer = KafkaConsumer(
@@ -13,7 +14,13 @@ def main():
         group_id='my-group',
         value_deserializer=lambda x: loads(x.decode('utf-8')))
 
+    for message in consumer:
+        # message value and key are raw bytes -- decode if necessary!
+        # e.g., for unicode: `message.value.decode('utf-8')`
+        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                             message.offset, message.key,
+                                             message.value))
+
 
 if __name__ == "__main__":
     main()
-    
