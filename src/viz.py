957dd7ae102f2d759ca3eff3b4a882684a38fae0
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-26 16:22:29 +0100

    add path

--- src/viz.py
+++ src/viz.py
@@ -5,8 +5,8 @@ import visens
 def main():
     """main"""
     path = "/data/kafka-to-nexus/"
-    filename = path + "nicos_00000757.hdf"
-    visens.tof(filename, save="/tmp/"+filename.replace(".hdf", ".png"))
+    filename = "nicos_00000757.hdf"
+    visens.tof(path+filename, save="/tmp/"+filename.replace(".hdf", ".png"))
 
 if __name__ == "__main__":
     main()
