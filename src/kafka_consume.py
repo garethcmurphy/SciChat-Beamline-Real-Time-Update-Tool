c6cc1e1d08316d237bcb4edf5ce50e25f0250cb6
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-29 10:48:37 +0100

    add check previous

--- src/kafka_consume.py
+++ src/kafka_consume.py
@@ -7,7 +7,6 @@ from kafka import KafkaConsumer, TopicPartition
 from scicat_bot import ScicatBot
 
 
-
 class KafkaManager:
     """add manager"""
     attrib = ""
@@ -15,8 +14,6 @@ class KafkaManager:
 
     def __init__(self):
         """init"""
-
-
 
     def consume(self):
         """setup consumer"""
@@ -35,34 +32,39 @@ class KafkaManager:
             # message value and key are raw bytes -- decode if necessary!
             # e.g., for unicode: `message.value.decode('utf-8')`
             print("%s:%d:%d: key=%s value=" % (message.topic, message.partition,
-                                            message.offset, message.key
-                                            ))
+                                               message.offset, message.key
+                                               ))
             val = message.value
             if "cmd" in val:
                 cmd = val["cmd"]
                 print(cmd)
                 if cmd == "FileWriter_new":
+                    self.previous_command = cmd
                     if "file_attributes" in val:
                         if "file_name" in val["file_attributes"]:
                             self.attrib = val["file_attributes"]
                             print(self.attrib["file_name"])
                 elif cmd == "FileWriter_stop":
-                    time.sleep(5)
-                    bot = ScicatBot()
-                    bot.login()
-                    proposal_id = "QHK123"
-                    room_alias = "#"+proposal_id+":ess"
-                    room_id = bot.get_room_id(room_alias)
-                    filename = self.attrib["file_name"]
-                    bot.post(room_id, filename)
-                    filename = "im.png"
-                    bot.upload_image(filename)
-                    bot.post_image(room_id)
+                    if (self.previous_command == "FileWriter_new"):
+                        self.previous_command = cmd
+                        time.sleep(5)
+                        bot = ScicatBot()
+                        bot.login()
+                        proposal_id = "QHK123"
+                        room_alias = "#"+proposal_id+":ess"
+                        room_id = bot.get_room_id(room_alias)
+                        filename = self.attrib["file_name"]
+                        bot.post(room_id, filename)
+                        filename = "im.png"
+                        bot.upload_image(filename)
+                        bot.post_image(room_id)
 
 
 def main():
-    manager =  KafkaManager()
+    """main"""
+    manager = KafkaManager()
     manager.consume()
+
 
 if __name__ == "__main__":
     main()
