5190be848e40f8cd1418211bf85a629a796416f7
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-29 10:46:10 +0100

    wait for stop before attempting plot

--- src/kafka_consume.py
+++ src/kafka_consume.py
@@ -1,49 +1,68 @@
 #!/usr/bin/env python3
 """kafa consume"""
+import time
 import json
+
 from kafka import KafkaConsumer, TopicPartition
 from scicat_bot import ScicatBot
 
 
+
+class KafkaManager:
+    """add manager"""
+    attrib = ""
+    previous_command = ""
+
+    def __init__(self):
+        """init"""
+
+
+
+    def consume(self):
+        """setup consumer"""
+        consumer = KafkaConsumer(
+            bootstrap_servers=['localhost:9093'],
+            enable_auto_commit=False,
+            value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+        partition = TopicPartition('V20_writerCommand', 0)
+        consumer.assign([partition])
+        consumer.seek_to_end()
+        last_offset = consumer.position(partition)
+        print(last_offset)
+        consumer.seek(partition=partition, offset=last_offset-5)
+
+        for message in consumer:
+            # message value and key are raw bytes -- decode if necessary!
+            # e.g., for unicode: `message.value.decode('utf-8')`
+            print("%s:%d:%d: key=%s value=" % (message.topic, message.partition,
+                                            message.offset, message.key
+                                            ))
+            val = message.value
+            if "cmd" in val:
+                cmd = val["cmd"]
+                print(cmd)
+                if cmd == "FileWriter_new":
+                    if "file_attributes" in val:
+                        if "file_name" in val["file_attributes"]:
+                            self.attrib = val["file_attributes"]
+                            print(self.attrib["file_name"])
+                elif cmd == "FileWriter_stop":
+                    time.sleep(5)
+                    bot = ScicatBot()
+                    bot.login()
+                    proposal_id = "QHK123"
+                    room_alias = "#"+proposal_id+":ess"
+                    room_id = bot.get_room_id(room_alias)
+                    filename = self.attrib["file_name"]
+                    bot.post(room_id, filename)
+                    filename = "im.png"
+                    bot.upload_image(filename)
+                    bot.post_image(room_id)
+
+
 def main():
-    """setup consumer"""
-    consumer = KafkaConsumer(
-        bootstrap_servers=['localhost:9093'],
-        enable_auto_commit=False,
-        value_deserializer=lambda x: json.loads(x.decode('utf-8')))
-    partition = TopicPartition('V20_writerCommand', 0)
-    consumer.assign([partition])
-    consumer.seek_to_end()
-    last_offset = consumer.position(partition)
-    print(last_offset)
-    consumer.seek(partition=partition, offset=last_offset-5)
-
-    for message in consumer:
-        # message value and key are raw bytes -- decode if necessary!
-        # e.g., for unicode: `message.value.decode('utf-8')`
-        print("%s:%d:%d: key=%s value=" % (message.topic, message.partition,
-                                           message.offset, message.key
-                                           ))
-        val = message.value
-        if "cmd" in val:
-            cmd = val["cmd"]
-            print(cmd)
-            if cmd == "FileWriter_new":
-                if "file_attributes" in val:
-                    if "file_name" in val["file_attributes"]:
-                        attrib = val["file_attributes"]
-                        print(attrib["file_name"])
-                        bot = ScicatBot()
-                        bot.login()
-                        proposal_id = "QHK123"
-                        room_alias = "#"+proposal_id+":ess"
-                        room_id = bot.get_room_id(room_alias)
-                        filename = attrib["file_name"]
-                        bot.post(room_id, filename)
-                        filename = "im.png"
-                        bot.upload_image(filename)
-                        bot.post_image(room_id)
-
+    manager =  KafkaManager()
+    manager.consume()
 
 if __name__ == "__main__":
     main()
