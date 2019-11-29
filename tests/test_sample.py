a1cb2ef61ad64e27fcb2a08931e8be2f7de7dae1
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-29 13:37:28 +0100

    add unit tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,8 @@
 # content of test_sample.py
-def inc(x):
-    return x + 1
+from scicat_bot import ScicatBot
 
 
 def test_answer():
-    assert inc(3) == 5
+    """test"""
+    bot = ScicatBot()
+    assert(isinstance(bot.base_url, str))
