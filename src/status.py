fa9e5759a97d7980454762bdcd41fe579cdd28d5
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-30 06:10:13 +0100

    laster file

--- src/status.py
+++ src/status.py
@@ -28,7 +28,7 @@ class KafkaManager:
         consumer.seek_to_end()
         last_offset = consumer.position(partition)
         print(last_offset)
-        consumer.seek(partition=partition, offset=14165573  )
+        consumer.seek(partition=partition, offset=13565573  )
 
         for message in consumer:
             # message value and key are raw bytes -- decode if necessary!
