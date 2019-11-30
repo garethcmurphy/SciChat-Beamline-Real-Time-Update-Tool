30846bdcd43b10d60b934cf234f4362dcb482b89
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-30 05:45:11 +0100

    print file

--- src/status.py
+++ src/status.py
@@ -49,6 +49,7 @@ class KafkaManager:
                     pass
                 elif type1 == "filewriter_status_master":
                     files = val["files"]
+                    print(files)
                     key = ""
                     key = list(files.values())[0]
                     self.file_name = key["filename"]
