8b0c909ba5cdea4686e0ec47250ddb1650b614d3
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-30 04:57:24 +0100

    add comment

--- src/status.py
+++ src/status.py
@@ -31,9 +31,9 @@ class KafkaManager:
             # message value and key are raw bytes -- decode if necessary!
             # e.g., for unicode: `message.value.decode('utf-8')`
             #print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                               message.offset, message.key,
-                                               message.value
-                                               ))
+            #                                   message.offset, message.key,
+            #                                   message.value
+            #                                   ))
             val = message.value
             if "code" in val:
                 print(val["code"])
