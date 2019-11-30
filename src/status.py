7205a7e0188b6ea3c1ec129665ab2fc44a41c4f4
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-30 04:52:34 +0100

    add string

--- src/status.py
+++ src/status.py
@@ -30,7 +30,7 @@ class KafkaManager:
         for message in consumer:
             # message value and key are raw bytes -- decode if necessary!
             # e.g., for unicode: `message.value.decode('utf-8')`
-            print("%s:%d:%d: key=%s value=" % (message.topic, message.partition,
+            print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                                message.offset, message.key,
                                                message.value
                                                ))
